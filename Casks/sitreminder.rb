f33df271f8d836817c3db523d711ba22c5718ea9
hugang <hugang@codans.com>
2025-05-06 21:58:40 +0800

    Fix URL interpolation

--- Casks/sitreminder.rb
+++ Casks/sitreminder.rb
@@ -2,7 +2,7 @@ cask "sitreminder" do
   version "1.0.0"
   sha256 "568d69fe14cff4ab51cda900a37ae3c7b4600cdd81f98cf9a332a8d84eb14386"
 
-  url "https://github.com/ihugang/sitreminder/releases/download/v\#{version}/SitReminder.dmg"
+  url "https://github.com/ihugang/sitreminder/releases/download/v#{version}/SitReminder.dmg"
   name "SitReminder"
   desc "Menu bar reminder app for programmers to stand up and move"
   homepage "https://github.com/ihugang/sitreminder"
